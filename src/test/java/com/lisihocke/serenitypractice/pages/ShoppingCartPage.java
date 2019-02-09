fc3907ecb5d61e1e5e639b9dee05d6c3cc63006e
lisihocke <lisihocke@users.noreply.github.com>
2019-02-09 01:49:46 +0100

    switched to AssertJ assertions; added option to ignore features or scenarios

--- src/test/java/com/lisihocke/serenitypractice/pages/ShoppingCartPage.java
+++ src/test/java/com/lisihocke/serenitypractice/pages/ShoppingCartPage.java
@@ -5,14 +5,16 @@ import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DefaultUrl("/index.php?controller=order")
 public class ShoppingCartPage extends PageObject {
     public void checkCartContainsProduct(String productId) {
         String productReference = Serenity.getCurrentSession().get("productReference").toString();
         String productPrice = Serenity.getCurrentSession().get("productPrice").toString();
 
-        assert find(By.jquery("#product_" + productId + " .cart_ref")).getText().equalsIgnoreCase("SKU : "+productReference);
-        assert find(By.jquery("#product_" + productId + " .cart_quantity_input")).getAttribute("value").equalsIgnoreCase("1");
-        assert find(By.jquery("#product_" + productId + " .price")).getText().equalsIgnoreCase(productPrice);
+        assertThat(find(By.jquery("#product_" + productId + " .cart_ref")).getText()).isEqualToIgnoringCase(("SKU : "+productReference));
+        assertThat(find(By.jquery("#product_" + productId + " .cart_quantity_input")).getAttribute("value")).isEqualToIgnoringCase("1");
+        assertThat(find(By.jquery("#product_" + productId + " .price")).getText()).isEqualToIgnoringCase(productPrice);
     }
 }
